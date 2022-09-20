cdc0c8eecd746f1abb3e91d02d2387ac2d4f92cc
KDerecLaptop <derec8495@gmail.com>
2022-09-20 11:05:49 +0200

    Manage IndexError for showSummary

--- server.py
+++ server.py
@@ -16,12 +16,16 @@ def index():
 
 @app.route("/showSummary", methods=["POST"])
 def showSummary():
-    club = [club for club in clubs if club["email"] == request.form["email"]][
-        0
-    ]
-    return render_template(
-        "welcome.html", club=club, competitions=competitions
-    )
+    try:
+        club = [
+            club for club in clubs if club["email"] == request.form["email"]
+        ][0]
+        return render_template(
+            "welcome.html", club=club, competitions=competitions
+        )
+    except IndexError:
+        flash("No account related to this email, try again.", "error")
+        return render_template("index.html"), 401
 
 
 @app.route("/book/<competition>/<club>")
