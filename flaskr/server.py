bd2d2f87f001c16a037d355a8de72c5c549ed5c5
Kévin Dérécusson <derec84@hotmail.fr>
2022-09-23 11:19:26 +0200

    Put flask application in a function

--- flaskr/server.py
+++ flaskr/server.py
@@ -2,66 +2,73 @@ from flask import Flask, render_template, request, redirect, flash, url_for
 from utils import loadClubs, loadCompetitions
 
 
-app = Flask(__name__)
-app.secret_key = "something_special"
+def create_app(test_config=None):
+    app = Flask(__name__)
+    app.secret_key = "something_special"
 
-competitions = loadCompetitions()
-clubs = loadClubs()
+    if test_config is None:
+        competitions = loadCompetitions()
+        clubs = loadClubs()
+    
+
+    @app.route("/")
+    def index():
+        return render_template("index.html")
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+    @app.route("/showSummary", methods=["POST"])
+    def showSummary():
+        try:
+            club = [
+                club for club in clubs if club["email"] == request.form["email"]
+            ][0]
+            return render_template(
+                "welcome.html", club=club, competitions=competitions
+            )
+        except IndexError:
+            flash("No account related to this email, try again.", "error")
+            return render_template("index.html"), 401
 
 
-@app.route("/showSummary", methods=["POST"])
-def showSummary():
-    try:
-        club = [
-            club for club in clubs if club["email"] == request.form["email"]
+    @app.route("/book/<competition>/<club>")
+    def book(competition, club):
+        foundClub = [c for c in clubs if c["name"] == club][0]
+        foundCompetition = [c for c in competitions if c["name"] == competition][0]
+        if foundClub and foundCompetition:
+            return render_template(
+                "booking.html", club=foundClub, competition=foundCompetition
+            )
+        else:
+            flash("Something went wrong-please try again")
+            return render_template(
+                "welcome.html", club=club, competitions=competitions
+            )
+
+
+    @app.route("/purchasePlaces", methods=["POST"])
+    def purchasePlaces():
+        competition = [
+            c for c in competitions if c["name"] == request.form["competition"]
         ][0]
-        return render_template(
-            "welcome.html", club=club, competitions=competitions
+        club = [c for c in clubs if c["name"] == request.form["club"]][0]
+        placesRequired = int(request.form["places"])
+        competition["numberOfPlaces"] = (
+            int(competition["numberOfPlaces"]) - placesRequired
         )
-    except IndexError:
-        flash("No account related to this email, try again.", "error")
-        return render_template("index.html"), 401
-
-
-@app.route("/book/<competition>/<club>")
-def book(competition, club):
-    foundClub = [c for c in clubs if c["name"] == club][0]
-    foundCompetition = [c for c in competitions if c["name"] == competition][0]
-    if foundClub and foundCompetition:
-        return render_template(
-            "booking.html", club=foundClub, competition=foundCompetition
-        )
-    else:
-        flash("Something went wrong-please try again")
+        flash("Great-booking complete!")
         return render_template(
             "welcome.html", club=club, competitions=competitions
         )
 
 
-@app.route("/purchasePlaces", methods=["POST"])
-def purchasePlaces():
-    competition = [
-        c for c in competitions if c["name"] == request.form["competition"]
-    ][0]
-    club = [c for c in clubs if c["name"] == request.form["club"]][0]
-    placesRequired = int(request.form["places"])
-    competition["numberOfPlaces"] = (
-        int(competition["numberOfPlaces"]) - placesRequired
-    )
-    flash("Great-booking complete!")
-    return render_template(
-        "welcome.html", club=club, competitions=competitions
-    )
+    # TODO: Add route for points display
 
 
-# TODO: Add route for points display
+    @app.route("/logout")
+    def logout():
+        return redirect(url_for("index"))
 
+    app.run(debug=True)
 
-@app.route("/logout")
-def logout():
-    return redirect(url_for("index"))
+if __name__ == "__main__":
+    create_app()
