e39cd5ce42cd77aaab987ca00f0d554f6fa5a302
KDerecLaptop <derec8495@gmail.com>
2022-09-28 16:49:48 +0200

    Define a path to open json files
    Before, there was problem to find json file in the path.
    For example, when running locust in performace_tests folder,
    FileNotFoundError was raise.
    Now, the path will be always correctly indicate.

--- flaskr/utils.py
+++ flaskr/utils.py
@@ -1,13 +1,17 @@
 import json
+import os
+
+
+path = os.path.dirname(os.path.abspath(__file__))
 
 
 def loadClubs():
-    with open("clubs.json") as c:
+    with open(path + "\\clubs.json") as c:
         listOfClubs = json.load(c)["clubs"]
         return listOfClubs
 
 
 def loadCompetitions():
-    with open("competitions.json") as comps:
+    with open(path + "\\competitions.json") as comps:
         listOfCompetitions = json.load(comps)["competitions"]
         return listOfCompetitions
